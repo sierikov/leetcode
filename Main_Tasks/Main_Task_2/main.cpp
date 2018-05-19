b846222a5077274b4ea60c72de642dcecfd2fa83
ArtemSer <artem.sierikov@gmail.com>
2018-05-19 10:51:16 +0300

    3 main task done

--- Main_Tasks/Main_Task_2/main.cpp
+++ Main_Tasks/Main_Task_2/main.cpp
@@ -5,39 +5,39 @@
 
 using namespace std;
 
-#define F_NAME "phone.dat"
+#define BASE "phone.dat"
 
 struct PhoneItem {
-    char fname[11];
-    char sname[21];
-    char phone[13];
+    char firstName[5];
+    char secondName[10];
+    char phone[10];
     PhoneItem *prev;
     PhoneItem *next;
 };
 
 char prompt_menu_item();
 
-int input_number(char *str, int strlen, char *info);
+int iNumber(char *str, int length, char *info);
 
-int input_name(char *str, int strlen, char *info);
+int iName(char *str, int length, char *info);
 
-void print_phonelist(PhoneItem *head);
+void showList(PhoneItem *head);
 
-void delete_all_items(PhoneItem *&head);
+void allDelete(PhoneItem *&head);
 
-int add_to_phonelist(PhoneItem *&head);
+int addToList(PhoneItem *&head);
 
-void search_in_phonelist(PhoneItem *head);
+void search(PhoneItem *head);
 
-void load_phonelist(PhoneItem *&head);
+void loadList(PhoneItem *&head);
 
-void save_phonelist(PhoneItem *head);
+void saveList(PhoneItem *head);
 
-int delete_phoneitem(PhoneItem *&head);
+int deleteItem(PhoneItem *&head);
 
-void sort_phonelist(PhoneItem *&head);
+void sortList(PhoneItem *&head);
 
-void sort_by_fname(PhoneItem *&tmphead);
+void sortFirstName(PhoneItem *&tempHead);
 
 int main(int argc, char *argv[]) {
     PhoneItem *Head = nullptr;
@@ -46,30 +46,30 @@ int main(int argc, char *argv[]) {
         variant = prompt_menu_item();
         switch (variant) {
             case '1':
-                add_to_phonelist(Head);
+                addToList(Head);
                 break;
             case '2':
-                delete_phoneitem(Head);
+                deleteItem(Head);
                 break;
             case '3':
-                search_in_phonelist(Head);
+                search(Head);
                 break;
             case '4':
-                print_phonelist(Head);
+                showList(Head);
                 break;
             case '5':
-                sort_phonelist(Head);
+                sortList(Head);
                 break;
             case '6':
-                save_phonelist(Head);
+                saveList(Head);
                 break;
             case '7':
-                delete_all_items(Head);
-                load_phonelist(Head);
+                allDelete(Head);
+                loadList(Head);
                 break;
             case '8':
-                delete_all_items(Head);
-                cout << "Завершение работы программы." << endl << "Нажмите любую клавишу." << endl;
+                allDelete(Head);
+                cout << "End of Program" << endl << "Press any button to continue" << endl;
                 return 0;
             default:
                 break;
@@ -82,28 +82,28 @@ char prompt_menu_item()//Меню
     char variant;
     do {
         cout << endl << endl;
-        cout << "Выберите вариант\n" << endl;
-        cout << " 1. Добавить запись\n"
-             << " 2. Удалить запись\n"
-             << " 3. Найти запись\n"
-             << " 4. Показать все записи\n"
-             << " 5. Сортировать записи\n"
-             << " 6. Сохранить в файл\n"
-             << " 7. Загрузить из файла\n"
-             << " 8. Выйти из программы\n"
-                "Ваш выбор = ";
+        cout << "Choose option\n" << endl;
+        cout << " 1. Add a Line\n"
+             << " 2. Delete a Line\n"
+             << " 3. Look for a Line\n"
+             << " 4. Display full List\n"
+             << " 5. Sort List\n"
+             << " 6. Save to File\n"
+             << " 7. Load from File\n"
+             << " 8. Eit\n"
+                "Option > ";
         cin >> variant;
     } while (!strchr("12345678", variant));
     return variant;
 }
 
 // INPUT NUMBER
-int input_number(char *str, int strlen, char *info) {
+int iNumber(char *str, int length, char *info) {
     int i = 0;
     char ch;
 
     cout << endl << info << endl << "-> ";
-    while (i < strlen) {
+    while (i < length) {
         cin >> ch;
         if ((ch == '\r') && (i >= 3)) break;
         if (ch >= '0' && ch <= '9') {
@@ -117,16 +117,15 @@ int input_number(char *str, int strlen, char *info) {
 }
 
 //INPUT
-int input_name(char *str, int strlen, char *info) {
+int iName(char *str, int length, char *info) {
     int i = 0;
     char ch;
-
     cout << endl << info << endl << "-> ";
-    while (i <= strlen) {
+    while (i <= length) {
         cin >> ch;
         if ((ch == '\r') && (i >= 1)) break;
-        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch == '-') || (ch >= 'А' && ch <= 'Я') ||
-            (ch >= 'а' && ch <= 'я')) {
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch == '-'))
+        {
             str[i] = ch;
             i++;
             cout << ch;
@@ -136,21 +135,21 @@ int input_name(char *str, int strlen, char *info) {
     return 0;
 }
 
-void print_phonelist(PhoneItem *head)//Вывод данных из справочника
+void showList(PhoneItem *head)//Вывод данных из справочника
 {
     PhoneItem *tmpItem = head;
     int count = 0;
 
-    cout << "Телефонный справочник:" << endl;
+    cout << "Full List:" << endl;
     while (tmpItem) {
-        cout << count + 1 << ". " << tmpItem->fname << " " << tmpItem->sname << " " << tmpItem->phone << endl;
+        cout << count + 1 << ". " << tmpItem->firstName << " " << tmpItem->secondName << " " << tmpItem->phone << endl;
         count++;
         tmpItem = tmpItem->next;
     }
-    cout << "Всего записей " << count << endl;
+    cout << "Total of Lines = " << count << endl;
 }
 
-void delete_all_items(PhoneItem *&head)//Удаление всех данных
+void allDelete(PhoneItem *&head)//Удаление всех данных
 {
     PhoneItem *tmpItem;
     while (head) {
@@ -165,23 +164,20 @@ void delete_all_items(PhoneItem *&head)//Удаление всех данных
     }
 }
 
-int add_to_phonelist(PhoneItem *&head)//Добавление
+int addToList(PhoneItem *&head)//Добавление
 {
     PhoneItem *tmpItem = new PhoneItem, *tmpSearch = head;
-    tmpItem->fname[0] = '\0';
-    tmpItem->sname[0] = '\0';
+    tmpItem->firstName[0] = '\0';
+    tmpItem->secondName[0] = '\0';
     tmpItem->phone[0] = '\0';
     tmpItem->prev = nullptr;
     tmpItem->next = nullptr;
-    input_name(tmpItem->fname, 11, const_cast<char *>("Введите имя"));
-    cout << endl;
-    input_name(tmpItem->sname, 21, const_cast<char *>("Введите фамилию"));
-    cout << endl;
-
-    input_number(tmpItem->phone, 13, const_cast<char *>("Введите номер телефона"));
+    iName(tmpItem->firstName, 5, const_cast<char *>("Enter the First Name"));
+    iName(tmpItem->secondName, 10, const_cast<char *>("Enter the Second Name"));
+    iNumber(tmpItem->phone, 10, const_cast<char *>("Enter the number"));
     while (tmpSearch) {
         if (strcmp(tmpSearch->phone, tmpItem->phone) == 0) {
-            cout << endl << "Запись с таким номером телефона уже существует.";
+            cout << endl << "ERROR: This number already exist in list";
             delete tmpItem;
             return 1;
         }
@@ -200,7 +196,7 @@ int add_to_phonelist(PhoneItem *&head)//Добавление
     return 0;
 }
 
-void search_in_phonelist(PhoneItem *head)//Поиск по фамилии или по номеру
+void search(PhoneItem *head)//Поиск по фамилии или по номеру
 {
     int count = 0;
     char tmpstr[21];
@@ -209,9 +205,9 @@ void search_in_phonelist(PhoneItem *head)//Поиск по фамилии или
 
     do {
         cout << endl << endl;
-        cout << "Выберите вариант поиска\n" << endl;
-        cout << " 1. Искать по фамилии\n"
-             << " 2. Искать по номеру телефона\n" << endl;
+        cout << "Options: \n" << endl;
+        cout << " 1. Search by surname\n"
+             << " 2. Search by number\n" << endl;
         cin >> variant;
     } while (!strchr("12", variant));
 
@@ -219,50 +215,50 @@ void search_in_phonelist(PhoneItem *head)//Поиск по фамилии или
 
 
     if (variant == 1) {
-        input_name(tmpstr, 21, const_cast<char *>("Введите фамилию для поиска"));
-        cout << endl << "Идет поиск ..." << endl;
+        iName(tmpstr, 21, const_cast<char *>("Please enter surname"));
+        cout << endl << "Searching ..." << endl;
 
         while (tmpSearch) {
-            if (strcmp(tmpSearch->sname, tmpstr) == 0) {
-                cout << count + 1 << ". " << tmpSearch->fname << " " << tmpSearch->sname << " " << tmpSearch->phone
+            if (strcmp(tmpSearch->secondName, tmpstr) == 0) {
+                cout << count + 1 << ". " << tmpSearch->firstName << " " << tmpSearch->secondName << " " << tmpSearch->phone
                      << endl;
                 count++;
             }
             tmpSearch = tmpSearch->next;
         }
     } else { // поиск по номеру телефона
-        input_number(tmpstr, 13, const_cast<char *>("Введите номер телефона для поиска"));
-        cout << endl << "Идет поиск ..." << endl;
+        iNumber(tmpstr, 13, const_cast<char *>("Enter please the number"));
+        cout << endl << "Searching ..." << endl;
         while (tmpSearch) {
             if (strcmp(tmpSearch->phone, tmpstr) == 0) {
-                cout << count + 1 << ". " << tmpSearch->fname << " " << tmpSearch->sname << " " << tmpSearch->phone
+                cout << count + 1 << ". " << tmpSearch->firstName << " " << tmpSearch->secondName << " " << tmpSearch->phone
                      << endl;
                 count++;
             }
             tmpSearch = tmpSearch->next;
         }
     }
-    cout << endl << "Найдено записей " << count << endl;
+    cout << endl << "Found Lines:  " << count << endl;
 };
 
-void load_phonelist(PhoneItem *&head)//Загрузка из файла
+void loadList(PhoneItem *&head)//Загрузка из файла
 {
     PhoneItem *tmpItem;
     int count = 0;
-    FILE *fin = fopen(F_NAME, "rt");
+    FILE *fin = fopen(BASE, "rt");
 
     if (fin != nullptr) {
         while (!feof(fin)) {
             tmpItem = new PhoneItem;
-            tmpItem->fname[0] = '\0';
-            tmpItem->sname[0] = '\0';
+            tmpItem->firstName[0] = '\0';
+            tmpItem->secondName[0] = '\0';
             tmpItem->phone[0] = '\0';
             tmpItem->prev = nullptr;
             tmpItem->next = nullptr;
 
-            fscanf(fin, "%p %p %p", &tmpItem->fname, &tmpItem->sname, &tmpItem->phone);
+            fscanf(fin, "%s %s %s", &tmpItem->firstName, &tmpItem->secondName, &tmpItem->phone);
 
-            if (strlen(tmpItem->fname) && strlen(tmpItem->sname) && strlen(tmpItem->phone)) {
+            if (strlen(tmpItem->firstName) && strlen(tmpItem->secondName) && strlen(tmpItem->phone)) {
                 if (head) {
                     head->prev = tmpItem;
                     tmpItem->next = head;
@@ -282,32 +278,32 @@ void load_phonelist(PhoneItem *&head)//Загрузка из файла
     }
 };
 
-void save_phonelist(PhoneItem *head)//Сохранить в файл
+void saveList(PhoneItem *head)//Сохранить в файл
 {
     PhoneItem *tmpItem = head;
     int count = 0;
-    FILE *fout = fopen(F_NAME, "wt");
+    FILE *fout = fopen(BASE, "wt");
 
 
     if (fout != nullptr) {
         while (tmpItem) {
-            fprintf(fout, "%s %s %s\n", tmpItem->fname, tmpItem->sname, tmpItem->phone);
+            fprintf(fout, "%s %s %s\n", tmpItem->firstName, tmpItem->secondName, tmpItem->phone);
             tmpItem = tmpItem->next;
             count++;
         }
         fclose(fout);
-        cout << endl << "Записано записей " << count << endl;
+        cout << endl << "Amount of written Data: " << count << endl;
     } else {
-        cout << endl << "Ошибка! Невозможно открыть файл " << endl;
+        cout << endl << "ERROR: Unable to close the file! " << endl;
     }
 };
 
-int delete_phoneitem(PhoneItem *&head)//Удаление записи
+int deleteItem(PhoneItem *&head)//Удаление записи
 {
     PhoneItem *tmpSearch = head;
-    char tmp_phone[13];
+    char * tmp_phone = const_cast<char *>("");
     tmp_phone[0] = '\0';
-    input_number(tmp_phone, 13, const_cast<char *>("Чтобы удалить запись, введите номер телефона"));
+    iNumber(tmp_phone, 13, const_cast<char *>("Please enter the phone number"));
     while (tmpSearch) {
         if (strcmp(tmpSearch->phone, tmp_phone) == 0) {
             if ((tmpSearch->prev) && (tmpSearch->next)) {
@@ -322,16 +318,16 @@ int delete_phoneitem(PhoneItem *&head)//Удаление записи
                 head = nullptr;
             }
             delete tmpSearch;
-            cout << endl << "Запись удалена" << endl;
+            cout << endl << "Line has been deleted" << endl;
             return 0;
         }
         tmpSearch = tmpSearch->next;
     }
-    cout << endl << "Телефон не найден" << endl;
+    cout << endl << "Number not found in the List" << endl;
     return 0;
 };
 
-void sort_phonelist(PhoneItem *&head)//Сортировка справочника
+void sortList(PhoneItem *&head)//Сортировка справочника
 {
     PhoneItem *tmpHead = head, *tmpPk1, *tmpPk2, *Prev1, *Next2;
     int n = 0, i, j;
@@ -341,7 +337,7 @@ void sort_phonelist(PhoneItem *&head)//Сортировка справочник
         tmpHead = tmpHead->next;
     }
 
-    cout << endl << "Идет сортировка ..." << endl;
+    cout << endl << "Searching ..." << endl;
 
 
     //Bubble Sort
@@ -350,7 +346,7 @@ void sort_phonelist(PhoneItem *&head)//Сортировка справочник
         for (j = 0; j < n - 1 - i; j++) {
             tmpPk1 = tmpHead;
             tmpPk2 = tmpHead->next;
-            if (strcmp(tmpPk1->sname, tmpPk2->sname) > 0) {
+            if (strcmp(tmpPk1->secondName, tmpPk2->secondName) > 0) {
                 Prev1 = tmpPk1->prev;
                 Next2 = tmpPk2->next;
 
@@ -368,25 +364,25 @@ void sort_phonelist(PhoneItem *&head)//Сортировка справочник
             }
             tmpHead = tmpHead->next;
         }
-        sort_by_fname(tmpHead);// Сортировка по имени
+        sortFirstName(tmpHead);// Сортировка по имени
     }
-    sort_by_fname(head);// Сортировка по имени
-    cout << endl << "Сортировка завершена " << endl;
+    sortFirstName(head);// Сортировка по имени
+    cout << endl << "List is Sorted " << endl;
 };
 
-void sort_by_fname(PhoneItem *&tmphead)//Сортировка по имени
+void sortFirstName(PhoneItem *&tempHead)//Сортировка по имени
 {
     PhoneItem *tmpItem;
     char tmpStr[32];
     // Сортировка по имени
-    tmpItem = tmphead;
+    tmpItem = tempHead;
     while (true) {
         if (tmpItem->next) {
-            if (strcmp(tmpItem->sname, tmpItem->next->sname) == 0) {// фамилии совпадают
-                if (strcmp(tmpItem->fname, tmpItem->next->fname) > 0) {
-                    strcpy(tmpStr, tmpItem->fname);
-                    strcpy(tmpItem->fname, tmpItem->next->fname);
-                    strcpy(tmpItem->next->fname, tmpStr);
+            if (strcmp(tmpItem->secondName, tmpItem->next->secondName) == 0) {// фамилии совпадают
+                if (strcmp(tmpItem->firstName, tmpItem->next->firstName) > 0) {
+                    strcpy(tmpStr, tmpItem->firstName);
+                    strcpy(tmpItem->firstName, tmpItem->next->firstName);
+                    strcpy(tmpItem->next->firstName, tmpStr);
                     // surname doesn't need to be copied
                     strcpy(tmpStr, tmpItem->phone);
                     strcpy(tmpItem->phone, tmpItem->next->phone);
