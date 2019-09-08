1eb6704eed479d2e3036432dad7b545d0c6c007c
ArtemSer <artem.sierikov@gmail.com>
2019-09-08 12:10:35 +0200

    TwoSum | Done

--- src/artemser/TwoSum.java
+++ src/artemser/TwoSum.java
@@ -1,4 +1,20 @@
 package artemser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int rest = target - nums[i];
+            if (map.containsKey(rest) && map.get(rest) != i) {
+                return new int[] { i, map.get(rest) };
+            }
+        }
+        return null;
+    }
 }
--- tests/artemser/TwoSumTest.java
+++ tests/artemser/TwoSumTest.java
@@ -1,5 +1,31 @@
 package artemser;
 
-public class TestTwoSum {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSumTest {
+    int[][] expectedResult;
+
+    @Before
+    public void setUp(){
+        expectedResult = new int[][]{
+                {0,1},
+                null,
+                {2,3},
+                {1,2}
+        };
+    }
+
+    @Test
+    public void twoSum() {
+        Assert.assertArrayEquals(expectedResult[0], TwoSum.twoSum(new int[]{2,7,11,15}, 9));
+        Assert.assertArrayEquals(expectedResult[1], TwoSum.twoSum(new int[]{2,7,11,15}, 100));
+        Assert.assertArrayEquals(expectedResult[2], TwoSum.twoSum(new int[]{7,7,11,15}, 26));
+        Assert.assertArrayEquals(expectedResult[3], TwoSum.twoSum(new int[]{2,7,33,34}, 40));
+    }
 
 }
