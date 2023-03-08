60cf090795480a35754bacf023503c1c996dacba
sierikov <sierikov@pm.me>
2023-03-08 16:35:59 +0100

    Fix 875. Koko Eating Bananas

--- src/main/scala/sierikov/leetcode/tasks/easy/KokoEatingBananas.scala
+++ src/main/scala/sierikov/leetcode/tasks/easy/KokoEatingBananas.scala
@@ -3,6 +3,19 @@ package sierikov.leetcode.tasks.easy
 import scala.annotation.tailrec
 
 object KokoEatingBananas {
+
+  /**
+    * Calculates the minimum integer k (speed) such that the monkey can eat all the bananas within h hours.
+    * 
+    * The lower bound is 1, the upper bound is the maximum number of bananas.
+    * Using binary search, the algorithm will narrow the speed to the appropriate value.
+    * 
+    * @param piles array of piles of bananas
+    * @param h number of hours
+    * @return the minimum integer k such that she can eat all the bananas within h hours.
+    * 
+    * @see [[https://leetcode.com/problems/koko-eating-bananas/]]
+    */
   def minEatingSpeed(piles: Array[Int], h: Int): Int = {
 
     @tailrec
@@ -14,9 +27,8 @@ object KokoEatingBananas {
 
       println(s"speed: $speed, hourForSpeed: $hourForSpeed, hours: $hours, upper: $upper, lower: $lower")
 
-      if (hourForSpeed <= hours ) iterate(hours, speed, lower)
+      if (hourForSpeed <= hours) iterate(hours, speed, lower)
       else iterate(hours, upper, speed + 1)
-
 
     }
 
@@ -25,10 +37,9 @@ object KokoEatingBananas {
 
   private def calcMidpoint(upper: Int, lower: Int): Int = lower + (upper - lower) / 2
 
-  private def calcNeededHoursFor(piles: Array[Int], speed: Int): Int = piles.foldLeft(0) {
-    case (hours, pile) =>
-      val bananas = pile.toDouble
-      val hour = (bananas / speed).ceil
-      hours + hour.toInt
+  private def calcNeededHoursFor(piles: Array[Int], speed: Int): Int = piles.foldLeft(0) { case (hours, pile) =>
+    val bananas = pile.toDouble
+    val hour = (bananas / speed).ceil
+    hours + hour.toInt
   }
 }
--- src/test/scala/sierikov/leetcode/tasks/easy/KokoEatingBananasTest.scala
+++ src/test/scala/sierikov/leetcode/tasks/easy/KokoEatingBananasTest.scala
@@ -2,10 +2,10 @@ package sierikov.leetcode.tasks.easy
 
 import org.scalatest.flatspec.AnyFlatSpec
 
-class KokoEatingBananasTest extends AnyFlatSpec{
+class KokoEatingBananasTest extends AnyFlatSpec {
   it should "pass basic tests" in {
-    assert(KokoEatingBananas.minEatingSpeed(Array(3,6,7,11), 8) == 4L)
-    assert(KokoEatingBananas.minEatingSpeed(Array(3,6,7,11), 8) == 4L)
-    assert(KokoEatingBananas.minEatingSpeed(Array(3,6,7,11), 8) == 4L)
+    assert(KokoEatingBananas.minEatingSpeed(Array(3, 6, 7, 11), 8) == 4L)
+    assert(KokoEatingBananas.minEatingSpeed(Array(3, 6, 7, 11), 8) == 4L)
+    assert(KokoEatingBananas.minEatingSpeed(Array(3, 6, 7, 11), 8) == 4L)
   }
 }
