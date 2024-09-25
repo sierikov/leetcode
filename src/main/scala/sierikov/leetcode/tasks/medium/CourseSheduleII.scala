b848fefda22264635a79a931a4932631a6637ccc
sierikov <sierikov@pm.me>
2024-09-25 10:41:46 +0200

    Add 210. Course Schedule II

---
+++ src/main/scala/sierikov/leetcode/tasks/medium/CourseSheduleII.scala
@@ -0,0 +1,37 @@
+package sierikov.leetcode.tasks.medium
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
+// Leetcode: https://leetcode.com/problems/course-schedule-ii/description/
+object CourseSheduleII {
+  def findOrder(numCourses: Int, prerequisites: Array[Array[Int]]): Array[Int] = {
+    val map: Map[Int, Array[Int]] = createMap(prerequisites)
+    val starting = Queue.apply(findStarting(numCourses, createMap(prerequisites)).toArray*)
+    iterateOverCourses(map, starting)
+  }
+
+  def createMap(prerequisites: Array[Array[Int]]): Map[Int, Array[Int]] = 
+    prerequisites.groupMap(_.apply(0))(_.tail.head)
+
+  def findStarting(n: Int, map: Map[Int, Array[Int]]): Set[Int] = 
+    (0 until n).filter(i => map.get(i).isEmpty).toSet
+
+  @scala.annotation.tailrec
+  def iterateOverCourses(
+    map: Map[Int, Array[Int]],
+    startingCourses: Queue[Int],
+    takenCourses: Vector[Int] = Vector.empty
+  ): Array[Int] = startingCourses.dequeueOption match
+    case Some((course, queue)) => {
+      val removedVertex = map.view.mapValues(_.filter(_ != course)).toMap
+      val (coursesWithNoDependencies, otherCourses) = removedVertex.partition(_._2.isEmpty)
+      iterateOverCourses(
+        otherCourses,
+        queue.enqueueAll(coursesWithNoDependencies.keySet),
+        takenCourses :+ course
+      )
+    }
+    case None if map.isEmpty => takenCourses.toArray
+    case None => Array.emptyIntArray
+}
---
+++ src/test/scala/sierikov/leetcode/tasks/medium/CourseSheduleIITest.scala
@@ -0,0 +1,26 @@
+package sierikov.leetcode.tasks.medium
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class CourseSheduleIITest extends AnyFlatSpec with Matchers {
+  import CourseSheduleII._
+
+  it should "pass example test" in {
+    val result = findOrder(
+      4,
+      Array(Array(1, 0), Array(2, 0), Array(3, 1), Array(3, 2))
+    )
+
+    result should (equal(Array(0, 2, 1, 3)) or equal(Array(0, 1, 2, 3)))
+  }
+
+  it should "detect cycles in the graph" in {
+    val result = findOrder(
+      4,
+      Array(Array(1, 0), Array(2, 0), Array(3, 1), Array(0, 3))
+    )
+
+    result shouldBe Array.emptyIntArray
+  }
+}
