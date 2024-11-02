32b8c6b1109e5d8e4c028ab1a2fd6789a8f9a9ac
sierikov <sierikov@pm.me>
2024-11-02 13:21:43 +0100

    Optimize 2490. Circular Sentence
    Removed unneeded if else

--- src/main/scala/sierikov/leetcode/tasks/easy/CircularSentence.scala
+++ src/main/scala/sierikov/leetcode/tasks/easy/CircularSentence.scala
@@ -3,11 +3,8 @@ package sierikov.leetcode.tasks.easy
 object CircularSentence {
   def isCircularSentence(s: String): Boolean = {
     val words = s.split(" ")
-    if (words.length == 1)
-      words.head.head == words.head.last
-    else
-      words.zip(words.tail :+ words.head).forall { case (a, b) =>
-        a.last == b.head
-      }
+    words.zip(words.tail :+ words.head).forall { case (a, b) =>
+      a.last == b.head
+    }
   }
 }
